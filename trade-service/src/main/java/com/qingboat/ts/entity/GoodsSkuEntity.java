9ecba46f136d1ac414128972012e27db81b35945
david816 <wwflier@sohu.com>
2021-03-23 11:13:24 +0800

    完善接口的实现

--- trade-service/src/main/java/com/qingboat/ts/entity/GoodsSkuEntity.java
+++ trade-service/src/main/java/com/qingboat/ts/entity/GoodsSkuEntity.java
@@ -1,6 +1,7 @@
 package com.qingboat.ts.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
@@ -14,16 +15,19 @@ public class GoodsSkuEntity {
     @TableId(type = IdType.AUTO)
     private Long id;
 
+
     private String name;
 
     private Integer price;
 
-    private Integer inventory;
+    private Integer inventory; //库存数
 
     private Long goodsId;
 
+    @TableField(select = false)
     private Date createdAt;
 
+    @TableField(select = false)
     private Date updatedAt;
 
 
