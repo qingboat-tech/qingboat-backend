60b50e555bb6d9d95aa5ab52a4058223a1016304
david816 <wwflier@sohu.com>
2021-04-27 13:28:00 +0800

    添加分表策略

--- article-service/src/main/java/com/qingboat/as/config/MybatisConfig.java
+++ article-service/src/main/java/com/qingboat/as/config/MybatisConfig.java
@@ -1,14 +1,16 @@
 package com.qingboat.as.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.handler.TableNameHandler;
 import com.baomidou.mybatisplus.extension.plugins.inner.DynamicTableNameInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.qingboat.as.config.db.MyDynamicTableNameInterceptor;
 import com.qingboat.as.config.db.MyTableNameHandler;
+import com.qingboat.as.entity.ArticleCommentEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
 
@@ -23,26 +25,24 @@ public class MybatisConfig {
         PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
         // 取消MyBatis Plus的最大分页500条的限制
         paginationInnerInterceptor.setMaxLimit(100000L);
-        MyDynamicTableNameInterceptor myDynamicTableNameInterceptor = new MyDynamicTableNameInterceptor();
 
-        HashMap<String, MyTableNameHandler> map = new HashMap<String, MyTableNameHandler>(2) {{
+        HashMap<String, TableNameHandler> map = new HashMap<String, TableNameHandler>(2) {{
             //整个函数返回的结果就是替换后的新表名，这个生成的表名的规则可以自己随便指定
 //            put("test_student", (sql, tableName, student) -> tableName + "_" +((SysStudent)student).getDepartCode());
 
-            put("apps_article_comment", new MyTableNameHandler() {
+            put("apps_article_comment", new TableNameHandler() {
                 @Override
-                public String dynamicTableName(String sql, String tableName, Object parameter) {
-                    log.debug(" SQL: "+ sql);
-                    log.debug(" tableName: "+ tableName);
-                    log.debug(" parameter: "+ parameter.toString());
-                    return tableName;
+                public String dynamicTableName(String sql, String tableName) {
+
+                    return tableName+"_1";
                 }
             });
         }};
 
         DynamicTableNameInnerInterceptor dynamicTableNameInnerInterceptor = new DynamicTableNameInnerInterceptor();
-        myDynamicTableNameInterceptor.setTableNameHandlerMap(map);
-        interceptor.addInnerInterceptor(myDynamicTableNameInterceptor);
+        dynamicTableNameInnerInterceptor.setTableNameHandlerMap(map);
+        interceptor.addInnerInterceptor(dynamicTableNameInnerInterceptor);
+
         interceptor.addInnerInterceptor(paginationInnerInterceptor);
 
         return interceptor;
