edef10841cca9e4d5d0e245ce0e5cde45ebe8b35
jerryan9999 <jerry.an@rentx.io>
2021-05-14 18:24:50 +0800

    fix service bug

--- article-service/src/main/java/com/qingboat/as/service/impl/UserWechatServiceImpl.java
+++ article-service/src/main/java/com/qingboat/as/service/impl/UserWechatServiceImpl.java
@@ -2,12 +2,13 @@ package com.qingboat.as.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.qingboat.as.dao.TierDao;
-import com.qingboat.as.entity.TierEntity;
-import com.qingboat.as.service.TierService;
+import com.qingboat.as.dao.UserWechatDao;
+import com.qingboat.as.entity.UserWechatEntity;
+import com.qingboat.as.service.UserWechatService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-public class UserWechatServiceImpl extends ServiceImpl<TierDao, TierEntity> implements TierService {
+public class UserWechatServiceImpl extends ServiceImpl<UserWechatDao, UserWechatEntity> implements UserWechatService {
 }
