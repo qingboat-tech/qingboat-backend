11e05f68bfc489a848f4f5f0051b0f4cdd981158
david816 <wwflier@sohu.com>
2021-05-15 16:43:00 +0800

    修改编译错误

--- article-service/src/main/java/com/qingboat/as/service/impl/ArticleServiceImpl.java
+++ article-service/src/main/java/com/qingboat/as/service/impl/ArticleServiceImpl.java
@@ -22,6 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.*;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -58,6 +59,7 @@ public class ArticleServiceImpl implements ArticleService {
     private FeishuService feishuService;
 
     @Autowired
+    @Lazy
     private MessageService messageService;
 
     @Autowired
