5838b9e99e93e10dfe4b408ab1c2c8aa6ee8e8ce
jerryan9999 <jerry.an@rentx.io>
2021-05-18 16:34:16 +0800

    fix insert scheduling

--- user-service/src/main/java/com/qingboat/us/UserServiceApplication.java
+++ user-service/src/main/java/com/qingboat/us/UserServiceApplication.java
@@ -6,12 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.qingboat")
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.qingboat")
 @MapperScan("com.qingboat.us.dao")
+@EnableScheduling
+@EnableAsync
 public class UserServiceApplication {
 
     public static void main(String[] args) {
