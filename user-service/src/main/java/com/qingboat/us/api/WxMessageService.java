741c77c565f67a9e6fcaee5e2b12f0333e818fb9
jerryan9999 <jerry.an@rentx.io>
2021-05-18 18:49:11 +0800

    wip

---
+++ user-service/src/main/java/com/qingboat/us/api/WxMessageService.java
@@ -0,0 +1,17 @@
+package com.qingboat.us.api;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "wx-service",url = "${wx.host}")
+public interface WxMessageService {
+
+    @RequestMapping(value = "/cgi-bin/message/template/send" , method = RequestMethod.POST)
+    Object sendMessage(@RequestParam("access_token") String accessToken, @RequestBody JSONObject body);
+
+
+}
