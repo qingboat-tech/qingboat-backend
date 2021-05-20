3b2bbaff2bbc28d68d60b36625533444988b4238
david816 <wwflier@sohu.com>
2021-05-20 16:21:29 +0800

    修改编译问题

--- article-service/src/main/java/com/qingboat/as/service/UserSubscriptionService.java
+++ article-service/src/main/java/com/qingboat/as/service/UserSubscriptionService.java
@@ -1,11 +1,7 @@
 package com.qingboat.as.service;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.qingboat.as.entity.CreatorBillEntity;
 import com.qingboat.as.entity.UserSubscriptionEntity;
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
 
 public interface UserSubscriptionService extends IService<UserSubscriptionEntity> {
 
