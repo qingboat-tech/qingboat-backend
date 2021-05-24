aa5271e8e7853de2d42f04e3de252debe6a79052
david816 <wwflier@sohu.com>
2021-05-24 18:07:11 +0800

    优化报警数据

--- core/src/main/java/com/qingboat/base/advise/ExceptionFeishuAlarmService.java
+++ core/src/main/java/com/qingboat/base/advise/ExceptionFeishuAlarmService.java
@@ -12,6 +12,8 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 @Component
 @Slf4j
@@ -28,47 +30,70 @@ public class ExceptionFeishuAlarmService {
 
     @Async
     public void alarmSystemException(Exception e){
-        FeishuService.PostBody postBody = new FeishuService.PostBody();
+        FeishuService.PostBody postBody = create(e);
         postBody.setTitle("系统异常");
-        FeishuService.PostTextTag textTag = new FeishuService.PostTextTag();
-        textTag.setText("用户Id:");
-        FeishuService.PostLinkTag linkTag = new FeishuService.PostLinkTag();
-        linkTag.setText(getUId()+"");
-        linkTag.setHref("https://creator.qingboat.com");
-
-        FeishuService.PostTextTag contentTag = new FeishuService.PostTextTag();
-        contentTag.setText(e.toString());
-
-        postBody.addContent(textTag,linkTag);
-        postBody.addContent(contentTag);
-
         feishuService.sendTextMsg(globExceptionHookKey,postBody);
     }
 
 
     @Async
     public void alarmBizException(BaseException e){
-        FeishuService.PostBody postBody = new FeishuService.PostBody();
+        FeishuService.PostBody postBody = create(e);
         postBody.setTitle("业务异常");
+        feishuService.sendTextMsg(bizExceptionHookKey,postBody);
+    }
+
+
+    private FeishuService.PostBody create(Exception e){
+        FeishuService.PostBody postBody = new FeishuService.PostBody();
+        postBody.setTitle("异常标题");
         FeishuService.PostTextTag textTag = new FeishuService.PostTextTag();
         textTag.setText("用户Id:");
         FeishuService.PostLinkTag linkTag = new FeishuService.PostLinkTag();
         linkTag.setText(getUId()+"");
         linkTag.setHref("https://creator.qingboat.com");
 
+        FeishuService.PostTextTag textTag1 = new FeishuService.PostTextTag();
+        textTag1.setText("请求路径:" +getRequestURI());
+
         FeishuService.PostTextTag contentTag = new FeishuService.PostTextTag();
-        contentTag.setText(e.toString());
+        contentTag.setText(getTrace(e));
 
         postBody.addContent(textTag,linkTag);
+        postBody.addContent(textTag1);
         postBody.addContent(contentTag);
 
-        feishuService.sendTextMsg(bizExceptionHookKey,postBody);
+        return postBody;
+    }
+
+    private String getTrace(Throwable t) {
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        t.printStackTrace(writer);
+        StringBuffer buffer = stringWriter.getBuffer();
+        return buffer.toString();
+    }
+
+
+    protected HttpServletRequest getRequest(){
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();//这个RequestContextHolder是Springmvc提供来获得请求的东西
+        if(requestAttributes !=null && requestAttributes instanceof ServletRequestAttributes){
+            return  ((ServletRequestAttributes)requestAttributes).getRequest();
+        }
+        return null;
+    }
+
+    protected String getRequestURI(){
+        HttpServletRequest request =getRequest();
+        if(request !=null){
+            return request.getRequestURI();
+        }
+        return null;
     }
 
     protected   Object getAttribute(String s) {
-        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();//这个RequestContextHolder是Springmvc提供来获得请求的东西
-        if(requestAttributes !=null && requestAttributes instanceof ServletRequestAttributes){
-            HttpServletRequest request = ((ServletRequestAttributes)requestAttributes).getRequest();
+        HttpServletRequest request =getRequest();
+        if(request !=null){
             return request.getAttribute(s);
         }
         return null;
