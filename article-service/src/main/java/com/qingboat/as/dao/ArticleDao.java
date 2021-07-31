5eb33c51ea75ab5de227620923c9f802595e46a0
lixin <542842297@qq.com>
2021-07-31 15:31:36 +0800

    article地址添加

--- article-service/src/main/java/com/qingboat/as/dao/ArticleDao.java
+++ article-service/src/main/java/com/qingboat/as/dao/ArticleDao.java
@@ -8,6 +8,6 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface ArticleDao {
-    @Select("select id,title,summary as `desc` from apps_article where id = #{articleId} and apps_article.accessible = 1 and is_private = 0  ")
+    @Select("select id,title,summary as `desc`,entry_url from apps_article where id = #{articleId} and apps_article.accessible = 1 and is_private = 0  ")
     public ArticleEntity selectArticleById(@Param("articleId")Integer articleId);
 }
