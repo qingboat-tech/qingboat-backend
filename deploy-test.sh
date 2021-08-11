7bf554cff30cdff9a76a0aa9726ba4f668afc248
lixin <542842297@qq.com>
2021-08-11 16:10:34 +0800

    测试

--- deploy-test.sh
+++ deploy-test.sh
@@ -13,7 +13,7 @@ done
 echo "SVAR set to: $SVAR"
 
 
-# 开始执行打包命令
+# 开始执行打包命令 ？？
 ssh $(whoami)@116.62.226.159 << soff
 sudo su - deploy<<EOF
 cd /srv/qingboat-backend
@@ -23,32 +23,32 @@ git pull git@github.com:qingboat-tech/qingboat-backend.git
 echo "=============git pull done=============="
 sleep 1
 EOF
-
-echo "=========build service==========="
-# cd /srv/qingboat-backend
-# sudo mvn clean install
-
-echo "=========service restart begin=========="
-
-case "$SVAR" in
-  ts)
-    sudo systemctl restart qingboat-ts.service
-      ;;
-  as)
-    sudo systemctl restart qingboat-as.service
-      ;;
-  us)
-    sudo systemctl restart qingboat-us.service
-      ;;
-  *)
-    sudo systemctl restart qingboat-ts.service
-    sudo systemctl restart qingboat-as.service
-    sudo systemctl restart qingboat-us.service
-      ;;
-  esac
-
-
-echo "========= service restart end =========="
-sleep 1
+#
+#echo "=========build service==========="
+## cd /srv/qingboat-backend
+## sudo mvn clean install
+#
+#echo "=========service restart begin=========="
+#
+#case "$SVAR" in
+#  ts)
+#    sudo systemctl restart qingboat-ts.service
+#      ;;
+#  as)
+#    sudo systemctl restart qingboat-as.service
+#      ;;
+#  us)
+#    sudo systemctl restart qingboat-us.service
+#      ;;
+#  *)
+#    sudo systemctl restart qingboat-ts.service
+#    sudo systemctl restart qingboat-as.service
+#    sudo systemctl restart qingboat-us.service
+#      ;;
+#  esac
+#
+#
+#echo "========= service restart end =========="
+#sleep 1
 exit
 soff
