26bc97b910711994db2409d99a3359fef1834e72
lixin <542842297@qq.com>
2021-08-30 19:37:00 +0800

    年订阅debug

--- article-service/src/main/java/com/qingboat/as/controller/CreatorSubscriptionController.java
+++ article-service/src/main/java/com/qingboat/as/controller/CreatorSubscriptionController.java
@@ -355,7 +355,9 @@ public class CreatorSubscriptionController extends BaseController {
     @PostMapping(value = "/tier")
     @ResponseBody
     public TierEntity addCreatorTierEntity(@RequestBody TierEntity tierEntity) {
+        System.out.println("System.out.println(tierEntity.getSubscribeDuration());");
         System.out.println(tierEntity.getSubscribeDuration());
+        System.out.println("========");
         if (StringUtils.isEmpty(tierEntity.getTitle())){
             throw new BaseException(500,"操作失败：创建会员等级标题为空");
         }
